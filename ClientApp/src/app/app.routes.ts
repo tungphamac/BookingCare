23e06b082a2c6dc740e9728cfe37054eab95be7c
tungphamac <tungpham.ac@gmail.com>
2025-03-28 11:23:06 +0700

    tungpt-commit
    add homepage-navbar-footer

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './core/components/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomepageComponent }
+];
