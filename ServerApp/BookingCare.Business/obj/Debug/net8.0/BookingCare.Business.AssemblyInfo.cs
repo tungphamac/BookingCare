1cd245019708eb7e5d2790c2f628cb610945fe01
TuanNA12312 <tuanna0865@gmail.com>
2025-03-31 15:26:42 +0700

    commit

--- ServerApp/BookingCare.Business/obj/Debug/net8.0/BookingCare.Business.AssemblyInfo.cs
+++ ServerApp/BookingCare.Business/obj/Debug/net8.0/BookingCare.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingCare.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff19966557debc269f95b4396d7d2d80c81a8c3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c042f0cb0e6559decfcf26c2f36ab85287e1869")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingCare.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingCare.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ServerApp/BookingCare.Data/obj/Debug/net8.0/BookingCare.Data.AssemblyInfo.cs
+++ ServerApp/BookingCare.Data/obj/Debug/net8.0/BookingCare.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingCare.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff19966557debc269f95b4396d7d2d80c81a8c3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c042f0cb0e6559decfcf26c2f36ab85287e1869")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingCare.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingCare.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
