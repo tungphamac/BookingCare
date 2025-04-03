c2ce0e522bb2c97987c3566006a111973c28540c
KhuongThanh <thanhkhuonghong1234@gmail.com>
2025-04-03 15:32:29 +0700

    abc
    abc

--- ServerApp/BookingCare.Data/obj/Debug/net8.0/BookingCare.Data.AssemblyInfo.cs
+++ ServerApp/BookingCare.Data/obj/Debug/net8.0/BookingCare.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingCare.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfa73cbc932247dc12361822794060e77f05c626")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b48e41505dcaacf285e1b05d5e06c578ee2d29d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingCare.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingCare.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
